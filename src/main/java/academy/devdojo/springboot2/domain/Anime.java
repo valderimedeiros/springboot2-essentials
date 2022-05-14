a9dcac4e63669d957e9a9ffa83b24dbfd04d3970
valderi <valderimedeiros@gmail.com>
2022-05-14 06:35:32 -0300

    Initial commit

---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,17 @@
+package academy.devdojo.springboot2.domain;
+
+public class Anime {
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
