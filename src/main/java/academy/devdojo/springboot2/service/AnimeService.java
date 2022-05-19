f62d1020d3e6db788fa5caac2208054cd6b16358
valderi <valderimedeiros@gmail.com>
2022-05-18 22:16:00 -0300

    Implementation of save method.

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -6,12 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private final List<Anime> animes = List.of(new Anime(1L, "DBZ"),new Anime(2L, "Berserk"));
+    private static final List<Anime> animes;
 //    private final AnimeRepository animeRepository;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"),new Anime(2L, "Berserk")));
+    }
 
     public List<Anime> listAll(){
         return animes;
@@ -22,5 +28,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,1000000));
+        animes.add(anime);
+        return anime;
     }
 }
