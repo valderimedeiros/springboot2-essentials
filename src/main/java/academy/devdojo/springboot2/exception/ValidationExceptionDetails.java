4202353996aefbf3c02cdfaa44b6e24be7ee0fa1
valderi <valderimedeiros@gmail.com>
2022-06-01 00:41:44 -0300

    Implementation of handler for field validating

--- src/main/java/academy/devdojo/springboot2/exception/ValidationExceptionDetails.java
+++ src/main/java/academy/devdojo/springboot2/exception/ValidationExceptionDetails.java
@@ -1,5 +1,6 @@
 package academy.devdojo.springboot2.exception;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
@@ -7,21 +8,8 @@ import java.util.Objects;
 
 @Getter
 @SuperBuilder
+@EqualsAndHashCode(callSuper = true)
 public class ValidationExceptionDetails extends ExceptionDetails{
     private final String fields;
     private final String fieldsMessage;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        ValidationExceptionDetails that = (ValidationExceptionDetails) o;
-        return Objects.equals(fields, that.fields) && Objects.equals(fieldsMessage, that.fieldsMessage);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), fields, fieldsMessage);
-    }
 }
